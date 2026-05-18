4c0a2b006897c3ab21dbfbdc7f4c66a8d1229aeb
Carlos Cunha <carlosrubim13@gmail.com>
2026-05-17 21:48:44 -0300

    feat: initialize ExemploMemoria project with Spring Boot structure, including basic product management functionality

---
+++ ExemploMemoria/src/main/java/dc/unifacef/ExemploMemoria/Controller/ProdutoController.java
@@ -0,0 +1,53 @@
+package dc.unifacef.ExemploMemoria.Controller;
+
+import dc.unifacef.ExemploMemoria.Model.Produto;
+import dc.unifacef.ExemploMemoria.Service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+@RestController // classe responsável por controlar requisições rest
+@RequestMapping("/produto") // response apenas solicitações do endpoint /produto
+public class ProdutoController {
+
+    // injeção de dependência
+    // permite chamar métodos com objetos não instanciados
+    @Autowired
+    ProdutoService service;
+
+    // requisição pelo verbo GET
+    @GetMapping
+    public ResponseEntity<ArrayList<Produto>> listar(){
+        // retorna OK ao cliente
+        // retorna a lista de produtos
+        return ResponseEntity.ok(service.listar());
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> criar(@RequestBody Produto produto){
+        Produto novo = service.criar(produto);
+        URI uri = URI.create("/produto/" + novo.getId());
+        return  ResponseEntity.created(uri).body(novo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remove(@PathVariable Long id){
+        if (service.remove(id)){
+            return ResponseEntity.noContent().build(); // removeu
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Produto> atualiza(@PathVariable Long id,
+                                            @RequestBody Produto novo){
+        Produto resposta = service.atualiza(id, novo);
+        // se atualizou o produto, retorna ele, senão retorna notfound
+        return (resposta != null) ? ResponseEntity.ok(resposta) :
+                ResponseEntity.notFound().build();
+    }
+}
---
+++ ExemploMemoria/src/main/java/dc/unifacef/ExemploMemoria/Service/ProdutoService.java
@@ -0,0 +1,42 @@
+package dc.unifacef.ExemploMemoria.Service;
+
+import dc.unifacef.ExemploMemoria.Model.Produto;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+@Service
+public class ProdutoService {
+    // vetor de produtos
+    ArrayList<Produto> produtos = new ArrayList<>();
+    // contador
+    private long contador = 1;
+    public ArrayList<Produto> listar(){
+        return this.produtos;
+    }
+
+    public Produto criar(Produto produto){
+        produto.setId(contador);
+        contador++; // prepara para a próxima inserção
+        this.produtos.add(produto);
+        return produto;
+    }
+
+    public boolean remove(Long id){
+        // removeIf faz o for, verificando se o id é igual ao id do array
+        // se for igual, ele remove e retorna true, se não remover, retorna false
+        return produtos.removeIf( p -> p.getId().equals(id));
+    }
+
+    public Produto atualiza(Long id, Produto novo){
+        novo.setId(id);
+        for(int i=0;i<produtos.size();i++){
+            if (produtos.get(i).getId().equals(id)){ // encontrou
+                produtos.set(i, novo); // atualiza o produto na lista
+                return novo; // retorna o novo produto
+            }
+        }
+        return null; // não encontrou
+    }
+}
